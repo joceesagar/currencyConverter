b2e42f80b25927e7e4ecaf939dee9d962c0db72a
joceesagar <joshisagarm2@gmail.com>
2024-10-03 15:40:12 +0545

    added currency rate state

--- components/body.tsx
+++ components/body.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { motion, Variants } from "framer-motion";
 import CustomDropDown from './customDropDown';
 import { Input } from "@/components/ui/input"
+import { Button } from './ui/button';
 
 
 
@@ -13,6 +14,10 @@ import { Input } from "@/components/ui/input"
 
 function Body() {
   const [isReady, setIsReady] = useState(false);
+  const [fromCurrency, setFromCurrency] = useState<string>("INR")
+  const [fromCurrencyRate, setFromCurrencyRate] = useState<number>(92.728091)
+  const [toCurrency, setToCurrency] = useState<string>("USD")
+  const [toCurrencyRate, setToCurrencyRate] = useState<number>(1.10449)
 
   useEffect(() => {
     // Set a timeout to change the state after 3 ms
@@ -24,8 +29,13 @@ function Body() {
     return () => clearTimeout(timer);
   }, []);
 
+  console.log("From:",fromCurrency)
+  console.log("FromRate:",fromCurrencyRate)
+  console.log("To:",toCurrency)
+  console.log("toRate:",toCurrencyRate)
+
   return (
-    <section className='h-full w-full flex justify-center items-center absolute inset-0 '>
+    <section className='h-full w-full flex justify-center items-center absolute inset-0'>
       {isReady &&
         <motion.div
           initial={{ opacity: 0, y: -1000 }}
@@ -34,19 +44,41 @@ function Body() {
             duration: 3,
             ease: [0.68, -0.55, 0.27, 1.55],
           }}
-          className='bg-custom-bg h-[530px] w-[785px] rounded-3xl backdrop-blur-md shadow-lg flex flex-col justify-center items-center gap-5 border-2 divide-solid border-white'>
+          className='bg-custom-bg h-auto p-5 w-[785px] rounded-3xl backdrop-blur-md shadow-lg flex flex-col justify-center items-center gap-5 border-2 divide-solid border-white'>
           <div className='h2 converter'>CURRENCY CONVERTER</div>
           <div className='w-auto h-auto flex flex-col gap-10'>
             <div className='flex gap-40'>
-              <CustomDropDown/>
-              <CustomDropDown/>
+              <CustomDropDown 
+              label='FROM' 
+              selectedCurrency={fromCurrency}
+              onCurrencySelect={
+                (currency,rate)=>
+                  {setFromCurrency(currency)
+                  setFromCurrencyRate(rate)}
+              }
+              />
+              <CustomDropDown 
+              label='TO' 
+              selectedCurrency={toCurrency}
+              onCurrencySelect={
+                (currency,rate)=>
+                  {setToCurrency(currency)
+                    setToCurrencyRate(rate)
+                  }
+              }
+              />
             </div>
-            <div >
-              <div><p className='p1 ml-8'>AMOUNT</p></div>
+            <div className='flex flex-col justify-center items-center'>
+              <div><p className='p1'>AMOUNT</p></div>
               <Input placeholder='Eg:2500'/>
             </div>
           </div>
-          <div className='bg-slate-800 w-[403px] h-[40px] mt-3'>are </div>
+          <div className=' bg-black w-full h-auto mt-3 flex justify-center items-center'>
+            <Button variant="outline">Button</Button>
+          </div>
+          <div>
+            <p className='p1'>The Result is</p>
+          </div>
         </motion.div>
       }
 
--- components/customDropDown.tsx
+++ components/customDropDown.tsx
@@ -10,6 +10,11 @@ import { ChevronDownIcon } from '@radix-ui/react-icons';
 import { ScrollArea } from "@/components/ui/scroll-area"
 import axios from 'axios';
 
+interface CustomDropDownProps {
+  label: string;
+  selectedCurrency: string
+  onCurrencySelect: (currency:string, rate:number) => void
+}
 
 // Define interfaces for the API response
 interface CurrencyRates {
@@ -24,79 +29,83 @@ interface ExchangeRatesApiResponse {
   rates: CurrencyRates;
 }
 
-const CustomDropDown = () => {
+const CustomDropDown = ({ label, selectedCurrency, onCurrencySelect }: CustomDropDownProps) => {
   const [data, setData] = useState<ExchangeRatesApiResponse | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-  const [currency, setCurrency] = useState<string>("USD")
+ 
 
-  useEffect(() => {
-    const fetchCurrencyData = async () => {
-      try {
-        const response = await axios.get<ExchangeRatesApiResponse>(
-          'https://api.exchangeratesapi.io/v1/latest',
-          {
-            params: {
-              access_key: process.env.NEXT_PUBLIC_CURRENCY_API_KEY, // Use the API key from .env.local
-            },
-          }
-        );
-        setData(response.data); // Set the rates to the state directly
-      } 
-      catch (error: unknown) {
-        if (axios.isAxiosError(error)) {
-          setError(error.response?.data?.message || error.message);
-        } else if (error instanceof Error) {
-          setError(error.message);
-        } else {
-          setError('An unknown error occurred');
-        }
-      } 
-      finally {
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchCurrencyData = async () => {
+  //     try {
+  //       const response = await axios.get<ExchangeRatesApiResponse>(
+  //         'https://api.exchangeratesapi.io/v1/latest',
+  //         {
+  //           params: {
+  //             access_key: process.env.NEXT_PUBLIC_CURRENCY_API_KEY, // Use the API key from .env.local
+  //           },
+  //         }
+  //       );
+  //       setData(response.data); // Set the rates to the state directly
+  //     }
+  //     catch (error: unknown) {
+  //       if (axios.isAxiosError(error)) {
+  //         setError(error.response?.data?.message || error.message);
+  //       } else if (error instanceof Error) {
+  //         setError(error.message);
+  //       } else {
+  //         setError('An unknown error occurred');
+  //       }
+  //     }
+  //     finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    // Call the function to fetch data
-    fetchCurrencyData();
-  }, []);
+  //   // Call the function to fetch data
+  //   fetchCurrencyData();
+  // }, []);
 
   return (
     <div className="flex flex-col gap-1">
       <div>
-        <p className="p1 ml-[55px]">FROM</p>
+        <p className="p1 ml-[55px]">{label}</p>
       </div>
       <div>
         <DropdownMenu>
-          <DropdownMenuTrigger className="dropdown flex gap-1 group">
-            <div className="h-auto w-auto h2 dropdown1">
-              <img src={`https://flagsapi.com/${currency.slice(0,2)}/shiny/64.png`} 
-              alt={`${currency}flag`} />
-              {currency}
+          <DropdownMenuTrigger className="dropdown flex gap-2 group mt-2">
+            <div>
+              <img src={`https://flagsapi.com/${selectedCurrency.slice(0, 2)}/shiny/64.png`}
+                alt={`${selectedCurrency}flag`} />
+
             </div>
-            <div className="h-auto w-auto mt-1 group-hover:translate-x-1 dropdown-hover:translate-x-1 transition-all">
-              <ChevronDownIcon width={30} height={30} />
+            <div className='flex mt-3'>
+              <div className="h-auto w-auto h2 dropdown1 flex gap-2">
+                {selectedCurrency}
+              </div>
+              <div className="h-auto w-auto mt-1 group-hover:translate-x-1 dropdown-hover:translate-x-1 transition-all">
+                <ChevronDownIcon width={30} height={30} />
+              </div>
             </div>
           </DropdownMenuTrigger>
 
           <DropdownMenuContent>
-          <ScrollArea className="h-[200px]">
-            {/* Handle loading and error states */}
-            {loading && <DropdownMenuItem>Loading...</DropdownMenuItem>}
-            {error && <DropdownMenuItem>{error}</DropdownMenuItem>}
+            <ScrollArea className="h-[200px]">
+              {/* Handle loading and error states */}
+              {loading && <DropdownMenuItem>Loading...</DropdownMenuItem>}
+              {error && <DropdownMenuItem>{error}</DropdownMenuItem>}
 
-            {/* Map through currency rates if data exists */}
-            {data &&
-              Object.entries(data.rates).map(([currency, rate]) => (
-                <DropdownMenuItem key={currency}>
-                  {currency} <img src={`https://flagsapi.com/${currency.slice(0,2)}/shiny/64.png`} 
-                  alt={`${currency}flag`} 
-                  onClick={()=>setCurrency(currency)}
-                  />
-                </DropdownMenuItem>
-              ))}
-              </ScrollArea>
-            <DropdownMenuSeparator />
+              {/* Map through currency rates if data exists */}
+              {data &&
+                Object.entries(data.rates).map(([currency, rate]) => (
+                  <DropdownMenuItem key={currency}>
+                    {currency} <img src={`https://flagsapi.com/${currency.slice(0, 2)}/shiny/64.png`}
+                      alt={`${currency}flag`}
+                      onClick={() => onCurrencySelect(currency,rate)}
+                    />
+                  </DropdownMenuItem>
+                ))}
+            </ScrollArea>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
